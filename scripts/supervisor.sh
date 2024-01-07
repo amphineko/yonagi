01ab59a2f13ea52675b3af5e1de1635cfde80294
amphineko <sukareki@gmail.com>
2024-01-07 23:52:46 +0000

    re: move packages into packages folder

--- scripts/supervisor.sh
+++ scripts/supervisor.sh
@@ -1,5 +1,5 @@
 set -ex
 
-cd "$(dirname "$(readlink -f "$0")")/../supervisor"
+cd "$(dirname "$(readlink -f "$0")")/../packages/supervisor"
 
 NODE_OPTIONS=--openssl-legacy-provider yarn ts-node src/main.ts | yarn pino-pretty
