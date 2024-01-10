870ee5c21ec27d14346b9fc4102714f2e8fdcba4
amphineko <sukareki@gmail.com>
2024-01-10 20:27:55 +0000

    fix(web): fix missing mandatory body when POSTing reload endpoint

--- packages/web/app/actions.ts
+++ packages/web/app/actions.ts
@@ -5,5 +5,5 @@ import * as t from "io-ts"
 import { postTypedEndpoint } from "../lib/actions"
 
 export async function reload(): Promise<void> {
-    await postTypedEndpoint(t.any, t.any, "api/v1/radiusd/reload", undefined)
+    await postTypedEndpoint(t.any, t.any, "api/v1/radiusd/reload", null)
 }
